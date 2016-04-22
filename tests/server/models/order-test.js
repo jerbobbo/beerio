80bb443ecc64c167f7fd0b80fd8d45962b8d7c0f
antkim003@gmail.com <antkim003@gmail.com>
2016-04-22 00:38:00 -0400

    order model finish with tests

---
+++ tests/server/models/order-test.js
@@ -0,0 +1,73 @@
+var dbURI    = 'mongodb://localhost:27017/testingDB';
+var clearDB  = require('mocha-mongoose')(dbURI); 
+
+var sinon    = require('sinon');
+var expect   = require('chai').expect;
+var mongoose = require('mongoose');
+
+// Require in all models.
+require('../../../server/db/models');
+
+var Order    = mongoose.model('Order');
+var User     = mongoose.model('User');
+var Lineitem = mongoose.model('Lineitem');
+var Promise  = require('bluebird');
+
+describe('Order Model', function () {
+
+  beforeEach('Establish DB connection', function (done) {
+      if (mongoose.connection.db) return done();
+      mongoose.connect(dbURI, done);
+  });
+
+  afterEach('Clear test database', function (done) {
+      clearDB(done);
+  });
+
+  describe('First Test', function () {
+    var _order, _user, _lineitem;
+    beforeEach(function (done) {
+      return Lineitem.create({
+        productId: null,
+        quantity: 3
+      })
+      .then(function(lineitem) {
+        _lineitem = lineitem;
+        return User.create({
+          email: 'test@test.com',
+        })
+      })  
+      .then(function(user) {
+        _user = user;
+        return Order.create({
+          user: user,
+          status: 'cart',
+          lineitems: [_lineitem]
+        })
+      })
+      .then(function(order) {
+        return order.populate('lineitems');
+      })
+      .then(function(populatedOrder) {
+        _order = populatedOrder;
+        done();
+      })
+      .catch(function(err) {
+        console.log('there was an error: ',err);
+        done();
+      })
+    });
+
+    it('should be an actual order', function () {
+      expect(_order).to.exist;
+      expect(_order.lineitems[0].quantity).to.equal(3);
+    });
+
+    afterEach(function (done) {
+      Promise.all([Order.findById(_order._id).remove(), User.findById(_user._id).remove(), Lineitem.findById(_lineitem._id).remove()])
+        .then(function(entries) {
+          done();
+        })
+    });
+  });
+});
