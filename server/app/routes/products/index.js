636bbd99c5955dc211cc094b110a63f1d74ce66e
John Nieves <karn09@gmail.com>
2016-04-22 22:50:53 -0400

    basic products route and test, dependent on product model

--- server/app/routes/products/index.js
+++ server/app/routes/products/index.js
@@ -1,11 +1,14 @@
 'use strict';
 var router = require('express').Router();
-var Product = require('../../../db/models/product');
-// var Product = {};
+var Product = require('mongoose').model('Product');
+
 router.get('/', function(req, res) {
   Product.find({})
     .then(function(products) {
       res.json(products);
+    })
+    .catch(function(err) {
+      res.json(err);
     });
 });
 
@@ -13,6 +16,9 @@ router.get('/products/:id', function(req, res) {
   Product.findById(req.params.id)
     .then(function(product) {
       res.json(product);
+    })
+    .catch(function(err) {
+      res.json(err);
     });
 });
 
--- tests/server/routes/products-routes-test.js
+++ tests/server/routes/products-routes-test.js
@@ -1,10 +1,31 @@
-var mongoose = require('mongoose');
-require('../../../server/db/models');
-
-var expect = require('chai').expect;
-
-var dbURI = 'mongodb://localhost:27017/testingDB';
-var clearDB = require('mocha-mongoose')(dbURI);
-
+// var expect = require('chai').expect;
 var supertest = require('supertest');
+var mongoose = require('mongoose');
+var Product = mongoose.model('Product');
+var dbURI = 'mongodb://localhost:27017/testingDB';
+var clearDB  = require('mocha-mongoose')(dbURI);
 var app = require('../../../server/app');
+var agent = supertest(app);
+
+describe('products route', function() {
+  beforeEach('Establish DB connection', function (done) {
+    if (mongoose.connection.db) return done();
+    mongoose.connect(dbURI, done);
+  });
+
+  afterEach('Clear test database', function (done) {
+    clearDB(done);
+  });
+
+  it('responds with 404 if route does not exixst on api.products', function(done) {
+    agent
+      .get('/api/products/kljasd')
+      .expect(404, done);
+  });
+
+  it('responds with 200 when retriving api.products', function(done) {
+    agent
+      .get('/api/products')
+      .expect(200, done);
+  });
+});
