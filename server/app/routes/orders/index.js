de90ec2c42efe7a2b17c3e2a00d75ece6b26c293
John Nieves <karn09@gmail.com>
2016-04-24 22:10:01 -0400

    changes to order get routes

--- server/app/routes/orders/index.js
+++ server/app/routes/orders/index.js
@@ -4,51 +4,57 @@ var Order = require('mongoose').model('Order');
 var Lineitem = require('mongoose').model('Lineitem');
 
 // find All  - probably not needed but helping with figuring out testing
+// modified this route to return all orders made by a particular user
 router.get('/', function(req, res) {
-
-  Order.find()
-    .then(function(orders) {
-      res.json(orders);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
+  if (req.user) {
+    Order.find({user: req.user._id})
+      .then(function(orders) {
+        res.json(orders);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  } else {
+    res.sendStatus(401)
+  }
 });
 
-//find by user
-router.get('user', function(req, res) {
-
-  Order.find({user:req.params.user})
-    .then(function(orders) {
-      res.json(orders);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
+// return specific order made by user. if unauth, return 401
+router.get('/:order_id', function(req, res) {
+  if (req.user) {
+    Order.findById(req.params.order_id)
+      .then(function(order) {
+        res.json(order);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  } else {
+    res.sendStatus(401);
+  }
 });
-
 
 // find orders by user
-router.get('/', function(req, res) {
+// router.get('/', function(req, res) {
 
-  Order.find({user:req.params.user})
-    .then(function(orders) {
-      res.json(orders);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
+//   Order.find({user:req.params.user})
+//     .then(function(orders) {
+//       res.json(orders);
+//     })
+//     .catch(function(err) {
+//       res.json(err);
+//     });
+// });
 
 //find by Id
-router.get('/:id', function(req, res) {
-  Order.findById(req.params.id)
-    .then(function(product) {
-      res.json(product);
-    })
-    .catch(function(err) {
-      res.json(err);
-    });
-});
+// router.get('/:id', function(req, res) {
+//   Order.findById(req.params.id)
+//     .then(function(product) {
+//       res.json(product);
+//     })
+//     .catch(function(err) {
+//       res.json(err);
+//     });
+// });
 
 module.exports = router;
