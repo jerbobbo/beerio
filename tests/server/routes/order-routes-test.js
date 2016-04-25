9c9eba47677ba83e999130a6d4586e4f6df61eb3
antkim003@gmail.com <antkim003@gmail.com>
2016-04-25 12:55:05 -0400

    created for #15 route for put api/orders/:id

--- tests/server/routes/order-routes-test.js
+++ tests/server/routes/order-routes-test.js
@@ -14,44 +14,6 @@ var Lineitem = mongoose.model('Lineitem');
 var Product  = mongoose.model('Product');
 var Promise  = require('bluebird');
 
-function seedFakeData() {
-  var _user, _product, _lineitem, _address;
-  return Promise.all([User.create({
-    email: 'testing123@test.com',
-    password: 'password'
-  }), Product.create({
-    name: 'Cerveza',
-    price: 10
-  }), Address.create({
-    type: 'shipping',
-    street: '500 9th ave',
-    city: 'new york',
-    state: 'NY',
-    country:'USA',
-    postal: '10019'
-  })]).spread(function(user, product, address) {
-    _user = user;
-    _product = product;
-    _address = address;
-    return Lineitem.create({
-      productId: _product._id,
-      quantity: 2
-    })
-    .then(function(lineitem) {
-      _lineitem = lineitem
-      var _data = {
-        user: _user._id,
-        lineitems: [_lineitem._id],
-        shippingAddress: _address._id,
-        billingAddress: _address._id,
-        status: 'cart'
-      }
-      return Promise.resolve(_data);
-    })
-    .catch(console.error);
-  });
-}
-
 describe('Orders API', function() {
   beforeEach('Establish DB connection', function (done) {
     if (mongoose.connection.db) return done();
@@ -62,19 +24,74 @@ describe('Orders API', function() {
     clearDB(done);
   });
 
+  
+
   describe('orders routes', function () {
-    var _order;
+    var _orderToBeCreated, _emptyorder, _user, _product, _lineitem, 
+        _address, _data, _createdOrder;
     beforeEach(function (done) {
-      seedFakeData()
-        .then(function(order) {
-          _order = order;
-          done();
-        });
+      return Promise.all([User.create({
+        email: 'testing123@test.com',
+        password: 'password'
+      }), Product.create({
+        name: 'Cerveza',
+        price: 10
+      }), Address.create({
+        type: 'shipping',
+        street: '500 9th ave',
+        city: 'new york',
+        state: 'NY',
+        country:'USA',
+        postal: '10019'
+      })])
+      .spread(function(user, product, address) {
+        _user = user;
+        _product = product;
+        _address = address;
+        return Lineitem.create({
+          productId: _product._id,
+          quantity: 2
+        })
+      })
+      .then(function(lineitem) {
+        _lineitem = lineitem
+
+        return Order.create({
+          status: 'cart'
+        })
+      })
+      .then(function(emptyOrder) {
+        _emptyorder = emptyOrder;
+        _orderToBeCreated = {
+          user: _user._id,
+          lineitems: [_lineitem._id],
+          shippingAddress: _address._id,
+          billingAddress: _address._id,
+          status: 'cart' 
+        }
+        return Order.create(_orderToBeCreated);
+      })
+      .then(function(createdOrder) {
+        console.log('this is the loaded created order', createdOrder);
+        _createdOrder = createdOrder
+        done();
+      })
+    });
+    afterEach(function (done) {
+      return Promise.all(
+        [
+          Order.remove(), User.remove(),
+          Product.remove(), Order.remove(),
+          Address.remove()
+        ])
+      .then(function(){
+        done();
+      })
     });
     // create an order with a user and a fake address
-    it('POST /api/orders route equals fake data', function(done) {
+    it('POST /api/orders route creates a fake order', function(done) {
       agent
-        .post('/api/orders/').send(_order)
+        .post('/api/orders/').send(_orderToBeCreated)
         .expect(200)
         .end(function(err,res) {
           if (err)return done(err);
@@ -82,5 +99,103 @@ describe('Orders API', function() {
           done();
         });
     });
+
+    it('PUT /api/orders/:orderId adding a lineitem', function (done) {
+      agent
+        .put('/api/orders/' + _emptyorder._id).send({
+          lineitems: [
+            {
+              productId: _product._id,
+              quantity: 4    
+            }
+          ]
+        })
+        .expect(200)
+        .end(function(err,res) {
+          if (err) return done(err);
+          expect(res.body.lineitems).to.exist;
+          done();
+        })
+    });
+
+
+    it('PUT /api/orders/:orderId modifying a lineitem', function (done) {
+      agent
+        .put('/api/orders/' + _createdOrder._id).send({
+          lineitems: [
+            {
+              productId: _product._id,
+              quantity: 4    
+            }
+          ]
+        })
+        .expect(200)
+        .end(function(err,res) {
+          if (err) return done(err);
+          expect(res.body.lineitems).to.exist;
+          done();
+        })
+    });
+
+    it('PUT /api/orders/:orderId adding a shipping or billing address', function (done) {
+      agent
+        .put('/api/orders/' + _emptyorder._id).send({
+          billingAddress: {
+            type: 'billing',
+            street: '500 9th ave',
+            city: 'new york',
+            state: 'NY',
+            country:'USA',
+            postal: '10019'
+          },
+          shippingAddress: {
+            type: 'shipping',
+            street: '500 9th ave',
+            city: 'new york',
+            state: 'NY',
+            country:'USA',
+            postal: '10019'
+          }
+        })
+        .expect(200)
+        .end(function(err,res) {
+          if (err) return done(err);
+          expect(res.body.billingAddress).to.exist;
+          expect(res.body.shippingAddress).to.exist;
+          done();
+        })
+    });
+
+    it('PUT /api/orders/:orderId modifying an existing shipping or billing address', function (done) {
+      agent
+        .put('/api/orders/' + _createdOrder._id).send({
+          billingAddress: {
+            type: 'billing',
+            street: '500 8th ave',
+            city: 'new york',
+            state: 'NY',
+            country:'USA',
+            postal: '10019'
+          },
+          shippingAddress: {
+            type: 'shipping',
+            street: '500 8th ave',
+            city: 'new york',
+            state: 'NY',
+            country:'USA',
+            postal: '10019'
+          }
+        })
+        .expect(200)
+        .end(function(err,res) {
+          if (err) return done(err);
+          expect(res.body.billingAddress).to.exist;
+          expect(res.body.billingAddress.street).to.equal('500 8th ave');
+          expect(res.body.shippingAddress).to.exist;
+          expect(res.body.shippingAddress.street).to.equal('500 8th ave');
+          done();
+        })
+    });
+
   });
 });
