e41246624a7bee2f9c5faa008fa10d724729ac6b
John Nieves <karn09@gmail.com>
2016-04-30 14:00:57 -0400

    Revert "Rd add to cart directive"

--- server/app/routes/cart/index.js
+++ server/app/routes/cart/index.js
@@ -5,6 +5,7 @@ var CartItem = require('mongoose').model('CartItem');
 
 router.get('/', function(req, res, next) {
   if (req.user) {
+
     Cart.findOne( {user: req.user._id} )
     .then(function(cart) {
       res.send(cart);
@@ -19,29 +20,42 @@ router.post('/', function(req, res, next) {
   if (req.user) {
     Cart.findOne({ user: req.user._id })
       .then(function(cart) {
-        var _item;
-        CartItem.create({
-          productId: req.body._id,
-          quantity: req.body.quantity
+        if (!cart) {
+          return Cart.create( {user: req.user._id });
+        }
+        return cart;
+      })
+      .then(function(cart) {
+        var found;
+        cart.items.forEach(function(item) {
+          if (req.body._id == item.productId._id) {
+            found = true;
+            CartItem.findByIdAndUpdate(item._id, { $inc: { quantity: 1 }})
+              .then(function(cartItem) {
+                res.json(cartItem);
+              })
+          }
         })
-        .then(function(item) {
-          _item = item;
-          cart.items.push(item);
-          return cart.save();
-        })
-        .then(function() {
-          return CartItem.findById(_item._id).populate('productId');
-        })
-        .then(function(_item) {
-          res.json(_item);
-        });
-      });
+        if (!found) {
+          return CartItem.create({
+            productId: req.body._id,
+            quantity: req.body.quantity
+          })
+          .then(function(item) {
+            cart.items.push(item)
+            cart.save()
+              .then(function(cart) {
+                res.json(cart);
+              })
+          })
+        }
+      })
   }
 });
 
 router.put('/:cartItemId', function(req, res, next) {
   if (req.user) {
-    CartItem.findById(req.params.cartItemId).populate('productId')
+    CartItem.findById(req.params.cartItemId)
     .then(function(cartItem) {
       cartItem.quantity = req.body.quantity;
       return cartItem.save();
