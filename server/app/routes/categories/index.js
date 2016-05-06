0ba3e8fe883486303d88ab6cc902f664ad0d451d
andrewmenendez <andrew@engrade.com>
2016-05-06 13:53:19 -0400

    get products by category route

--- server/app/routes/categories/index.js
+++ server/app/routes/categories/index.js
@@ -1,6 +1,7 @@
 'use strict';
 var router = require('express').Router();
 var Category = require('mongoose').model('Category');
+var Product = require('mongoose').model('Product');
 
 
 router.get('/', function(req, res, next) {
@@ -18,6 +19,31 @@ router.get('/:id', function(req, res, next) {
     .then(function(cat) {
       console.log('cat: ', cat);
       res.json(cat);
+    })
+    .catch(function(err) {
+      res.send(404).send(err);
+    }, next);
+});
+
+router.get('/:id/products', function(req, res, next) {
+  var _cat;
+  Category.findById(req.params.id)
+    .then(function(cat) {
+      console.log(cat)
+      _cat=cat;
+      return Product.find().populate('category')
+    })
+    .then(function(products){
+      var match_prods=products.filter(function(prod){
+        var isInArray = prod.category.some(function (category) {
+              return category.equals(_cat._id);
+          });
+        return isInArray;
+      })
+      return match_prods;
+    })
+    .then(function(matches){
+      res.json(matches);
     })
     .catch(function(err) {
       res.send(404).send(err);
