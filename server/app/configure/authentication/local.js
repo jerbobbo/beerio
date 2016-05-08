f3ab2daea39492bf8c5ec112747b7aa5edf4f905
John Nieves <karn09@gmail.com>
2016-05-08 19:47:06 -0400

    attempts at fixing cart recon

--- server/app/configure/authentication/local.js
+++ server/app/configure/authentication/local.js
@@ -5,17 +5,15 @@ var mongoose = require('mongoose');
 var User = mongoose.model('User');
 var Cart = mongoose.model('Cart');
 var CartItem = mongoose.model('CartItem');
-var Promise = require('bluebird');
+var Promise  = require('bluebird');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
     // When passport.authenticate('local') is used, this function will receive
     // the email and password to run the actual authentication logic.
-    var strategyFn = function(email, password, done) {
-        User.findOne({
-            email: email
-        })
-            .then(function(user) {
+    var strategyFn = function (email, password, done) {
+        User.findOne({ email: email })
+            .then(function (user) {
                 // user.correctPassword is a method from the User schema.
                 if (!user || !user.correctPassword(password)) {
                     done(null, false);
@@ -29,26 +27,23 @@ module.exports = function(app) {
 
     // middle ware for creating a cart
     app.use(function(req, res, next) {
-        if (!req.user && !req.session.cart) {
-            Cart.create({})
-                .then(function(cart) {
-                    req.session.cart = cart;
-                    next();
-                });
-
-        } else {
-            next();
-        }
+      if (!req.user && !req.session.cart) {
+        Cart.create({})
+        .then(function(cart) {
+          req.session.cart = cart;
+          next();
+        });
+        
+      } else {
+        next();
+      }
     });
 
-    passport.use(new LocalStrategy({
-        usernameField: 'email',
-        passwordField: 'password'
-    }, strategyFn));
+    passport.use(new LocalStrategy({ usernameField: 'email', passwordField: 'password' }, strategyFn));
 
     // A POST /login route is created to handle login.
-    app.post('/login', function(req, res, next) {
-        var authCb = function(err, user) {
+    app.post('/login', function (req, res, next) {
+        var authCb = function (err, user) {
 
             if (err) return next(err);
 
@@ -59,50 +54,48 @@ module.exports = function(app) {
             }
 
             // req.logIn will establish our session.
-            req.logIn(user, function(loginErr) {
+            req.logIn(user, function (loginErr) {
                 if (loginErr) return next(loginErr);
                 // We respond with a response object that has user with _id and email.
-                Cart.findOne({
-                    user: req.user._id
-                })
-                    .then(function(loggedInCart) {
-                        if (!loggedInCart) {
-                            return Cart.findById(req.session.cart._id)
-                                .then(function(cart) {
-                                    cart.user = req.user;
-                                    return cart.save();
-                                });
-                        }
-                        var combinedArr = [];
-                        for (var i = 0; i < loggedInCart.items.length; i++) {
-                            for (var j = 0; j < req.session.cart.items.length; j++) {
-                                if (loggedInCart.items[i]._id === req.session.cart.items[j]._id || req.session.cart.items[j].productId == loggedInCart.items[i].productId._id) {
-                                    // take the greater of the two quantities
-                                    if (loggedInCart.items[i].quantity >= req.session.cart.items[j].quantity) {
-                                        combinedArr.push(loggedInCart.items[i]);
-                                        break;
-                                    }
-                                    if (loggedInCart.items[i].quantity <= req.session.cart.items[j].quantity) {
-                                        combinedArr.push(req.session.cart.items[j]);
-                                        break;
-                                    }
+                Cart.findOne({user: req.user._id})
+                .then(function(loggedInCart) {
+                    if (!loggedInCart) {
+                        return Cart.findById(req.session.cart._id)
+                            .then(function(cart) {
+                                cart.user = req.user;
+                                return cart.save();
+                            });
+                    }
+                    var combinedArr = [];
+                    for (var i = 0; i < loggedInCart.items.length; i++) {
+                        for (var j = 0; j < req.session.cart.items.length; j++) {
+                            if (loggedInCart.items[i]._id === req.session.cart.items[j]._id ||  req.session.cart.items[j].productId == loggedInCart.items[i].productId._id) {
+                                // take the greater of the two quantities
+                                if (loggedInCart.items[i].quantity >= req.session.cart.items[j].quantity) {
+                                    combinedArr.push(loggedInCart.items[i]);
+                                    break;
                                 }
-                                combinedArr.push(loggedInCart.items[i]);
-                                combinedArr.push(req.session.cart.items[j]);
+                                if (loggedInCart.items[i].quantity < req.session.cart.items[j].quantity) {
+                                    combinedArr.push(req.session.cart.items[j]);
+                                    break;
+                                }
                             }
+                            combinedArr.push(loggedInCart.items[i]);
+                            combinedArr.push(req.session.cart.items[j]);
                         }
-                        console.log(combinedArr)
-                        console.log('================FOUND CART==========', loggedInCart)
-                        loggedInCart.items = combinedArr;
-                        req.session.cart = null;
-                        return loggedInCart.save()
-                    })
-                    .then(function(finishedCart) {
-                        console.log('finished', finishedCart)
-                        res.status(200).send({
-                            user: user.sanitize()
-                        });
-                    })
+                    }
+                    console.log(combinedArr)
+                    console.log('================FOUND CART==========', loggedInCart)
+                    loggedInCart.items = combinedArr;
+                    req.session.cart = null;
+                    return loggedInCart.save()
+                })
+                .then(function(finishedCart) {
+                    console.log('finished', finishedCart)
+                    res.status(200).send({
+                        user: user.sanitize()
+                    });
+                })
             });
 
         };
