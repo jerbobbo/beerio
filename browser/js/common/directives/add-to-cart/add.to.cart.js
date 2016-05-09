1bf7312ab4e9a22ea7a122a3ccdeb9dfe0b8e9e3
antkim003@gmail.com <antkim003@gmail.com>
2016-05-09 16:31:26 -0400

    closes #160

--- browser/js/common/directives/add-to-cart/add.to.cart.js
+++ browser/js/common/directives/add-to-cart/add.to.cart.js
@@ -5,11 +5,18 @@ app.directive('addToCart', function(AuthService, CartFactory) {
     scope: {
       product: '=',
       lineItem: '=',
-      label: '@'
+      label: '@',
+      detail: '='
     },
-    link: function (scope) {
+    link: function (scope, link, attr) {
       scope.isLoggedIn = AuthService.isAuthenticated;
-      scope.addToCart = CartFactory.addToCart;
+      scope.addToCart = function(product, ev, detail) {
+        CartFactory.addToCart(product).then(function(cart) {
+          if (detail) {
+            ev.target.innerHTML = "Added to cart (" + cart.quantity + ")";
+          }
+        })
+      };
       scope.updateQty = CartFactory.updateQty;
       scope.removeItem = CartFactory.removeItem;
       scope.getLineItem = CartFactory.getLineItem;
