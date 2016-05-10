b5c926d84f693e2c7f5e34cf2640a729993e18f3
John Nieves <karn09@gmail.com>
2016-05-10 20:55:27 +0000

    email at interval

--- server/cronjob/index.js
+++ server/cronjob/index.js
@@ -1,11 +1,17 @@
 var CronJob = require('cron').CronJob;
-var date = new Date(Date.now() + 60000);
-var job = new CronJob(date, sendMail, finish, true, 'America/Los_Angeles');
+var sendgrid = require('../sendgrid');
 
-function sendMail() {
-    console.log(Date.now())
-}
 function finish() {
     console.log('stopping job')
     job.stop()
+}
+
+module.exports = {
+    cronMail: function(email) {
+        var body = 'Your order has shipped. Enjoy your sips';
+        var subj = 'Beer.io - It\'s on the way!';
+        console.log('Starting cron...');
+        var date = new Date(Date.now() + 60000);
+        var job = new CronJob(new Date(Date.now() + 60000), sendgrid.mailTo(email, subj, body), finish, true);
+    }
 }
